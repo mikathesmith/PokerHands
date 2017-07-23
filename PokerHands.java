d036c126cf131739cafc24f4c11903ea9f46c9ba
Mika Smith <mika.e.smith@gmail.com>
2017-07-23 18:04:50 +1200

    Sorting correctly. Now accounting for cases if I have a duplicate card or 5 of the same number, if I am using one type of seperator and to get rid of the need to have a space after the line by using a count

--- PokerHands.java
+++ PokerHands.java
@@ -7,14 +7,15 @@ import javax.smartcardio.Card;
 
 public class PokerHands{
 	
-	private final int[] order;
-	private HashMap<Character, Integer> nums = new HashMap<Character, Integer>(); 
-	private ArrayList<Character> suits = new ArrayList<Character>(); 
-	private ArrayList<Character> seperators = new ArrayList<Character>();
-	//private ArrayList<Card<Integer, Character>> hand = new Arrayist<Card<Integer, Character>>());
+	private static int[] order;
+	private static HashMap<Character, Integer> nums = new HashMap<Character, Integer>(); 
+	private static ArrayList<Character> suits = new ArrayList<Character>(); 
+	private static ArrayList<Character> seperators = new ArrayList<Character>();
+	public static ArrayList<Card<Integer, Character>> hand;
 	
 	public PokerHands(){
 		order = new int[]{2,3,4,5,6,7,8,9,10,11,12,13,1};
+		hand = new ArrayList<Card<Integer, Character>>();
 		
 		nums.put('A',1);
 		nums.put('J', 11);
@@ -30,83 +31,122 @@ public class PokerHands{
 		seperators.add('/');
 		seperators.add('-');
 	}
-
-	public ArrayList<Card<Integer, Character>> sortHand(ArrayList<Card<Integer, Character>> hand){
-		ArrayList<Card<Integer, Character>> sortedHand = new ArrayList<Card<Integer, Character>>();
-		
-		//sort by number - find min number from 2 to 13
-		for(Card<Integer, Character> c: hand){ //for every card in the hand 
-			
-			if(sortedHand.isEmpty()){
-				sortedHand.add(c); //add the card straight to the sorted hand if its empty
-			}else{ 
-				int num = c.getNumber();//get the cards number
-				char suit = c.getSuit(); 
-				Boolean added = false; 
-				//System.out.print(num + "" + c.getSuit()+" ");
-				int index = 0; 
-				
-				for(Card<Integer, Character> i: sortedHand){
-					int numComparison = compareNumValue(num, i.getNumber());
-					
-					//if our card is less than a card in the sorted Hand
-					if(numComparison == -1){ //if number is less than i
-						sortedHand.add(index, c); //insert it at that index! 
-						added=true;
-						break; 
-					}else if(numComparison == 0){ //number is same as i
-						//look at suit!
-						int suitComparison = compareSuit(suit, i.getSuit());
-						if(suitComparison == -1){//our suit is less than the other 
-							//sortedHand.add(index, c); 
-							System.out.println("card is less value");
-						}else{ //CANNOT be equal, so must be greater than. 
-							//sortedHand.add(index+1, c);
-							System.out.println("card is greater value");
-							//swap cards 
-						}
-						
-					}
-					
-					//if it is equal, we will need to compare the suits. 
-					index++;
-				}
-				
-				if(!added){
-					sortedHand.add(c);
-				}
-			}
-			
-			//Print everything in the sorted hand so far! 
-
-		/*	for(Card<Integer, Character> x: sortedHand){
-				System.out.print(x.getNumber() + "" + x.getSuit()+" ");
-			}
-			System.out.println();*/
-			
-			//find the right place in the arraylist to place it! 
-		}
-		
-		
-		//sort by suit 
-		return sortedHand; 
-	}
 	
-/*	public void swap(Card x, Card, y){
-		int indexX = hand. 
-	}*/
-	
-	public boolean isSeperator(char c){
+	public static boolean isSeperator(char c){
 		return seperators.contains(c);
 	}
 	
-	public int compareSuit(char x, char y){
+	public static void main(String[]args){
+		
+		Scanner sc = new Scanner(System.in);
+		while(sc.hasNextLine()){
+			PokerHands poker = new PokerHands(); 
+			String line = sc.nextLine(); 
+			
+			Card<Integer,Character> card;
+			Boolean valid = true; 
+			String numString="";
+			//Random initialising. Must reset after each card! 
+			int num=0;
+			char suit='#'; 
+			//TODO: NOT SCANNING IN LAST ONE DUE TO NOT FINDING A SEPERATOR- after count 5 then stop? 
+		//	boolean isThisSeperator; to stick to one type of seperator
+			
+			for(int i=0; i<line.length();i++){
+				char c = line.charAt(i); //scan in every char 
+				
+				if(Character.isDigit(c)){ 
+					numString += c; //add to current number as it could be a double digit
+					num = Integer.parseInt(numString);
+				}else if(nums.containsKey(c)){
+					num = nums.get(c); 
+				}else if(suits.contains(Character.toUpperCase(c))){
+					suit = Character.toUpperCase(c);
+				}else if(isSeperator(c)){
+					
+				//	System.out.println("Found seperator");
+					card = new Card<Integer,Character>(num, suit); 
+			//TODO: this does not work as cards have different hashcodes. 
+					if(hand.contains(card)){ //hand doesn't contain this card
+						System.out.println("Duplicate card!");
+						valid = false; 
+					}else{
+						hand.add(card);
+					}
+					
+				//	System.out.println("Hand size is "+ hand.size());
+					//Resetting variables
+					num = 0;
+					numString = "";
+					suit = '#';
+				}else{
+					valid = false; 
+				}
+				
+			
+			}
+			
+			if(hand.size()!=5 || !valid){
+				System.out.println("Invalid: " + line);
+			}else{
+			/*	System.out.println("Unsorted: ");
+				for(Card<Integer, Character> c: hand){
+					System.out.print(c.getNumber() + "" + c.getSuit()+" ");
+				}
+				System.out.println();*/
+				sortHand();
+				
+				//System.out.println("\nSorted: ");
+				for(Card<Integer, Character> c: hand){
+					System.out.print(c.getNumber() + "" + c.getSuit()+" ");
+				}
+				System.out.println();
+				System.out.println();
+			}				
+		}
+		
+		sc.close();
+	}
+	
+	/*
+	 * Uses insertion sort to sort the hand as it is a small set of numbers with a 
+	 * small range. 
+	 */
+	public static void sortHand(){    
+		//for each value in the hand. 
+		for(int i=1; i < hand.size(); i++){
+	        Card<Integer, Character> value = hand.get(i);
+	        int j = i-1;
+	        while(j>=0 && compare(value, hand.get(j))==-1){
+	        	//Swaps values. 
+	        	Card<Integer, Character> temp = hand.get(j);
+	        	hand.set(j, hand.get(j+1));
+	        	hand.set(j+1, temp);
+	        	j--;
+	        }
+	    }   
+	}
+	
+	/*
+	 * Compares two cards and returns -1 if x is less than y, 0 if they are equal and
+	 * 1 if y is greater than x. 
+	 */
+	public static int compare(Card<Integer, Character> x, Card<Integer, Character> y){
+		if(x.getNumber() != y.getNumber()){
+			return compareNumValue(x.getNumber(), y.getNumber());
+		}else{
+			return compareSuit(x.getSuit(), y.getSuit());
+		}
+	}
+	
+	/* Compares two suits and returns -1 if x is less than y, 0 if they are equal and
+	 * 1 if y is greater than x. 
+	 */
+	public static int compareSuit(char x, char y){
 		int xcount = 0; 
 		int ycount = 0; 
 		boolean xfin = false;
 		boolean yfin = false; 
-		
-		System.out.println("Comparing " + x + " and " + y);
 		
 		for(char c : suits){
 			if(x == c){ 
@@ -125,10 +165,7 @@ public class PokerHands{
 				}
 			}
 		}
-		
-		System.out.println("xcount "+ xcount);
-		System.out.println("ycount "+ ycount);
-		
+	
 		if(xcount==ycount){
 			return 0; 
 		}else if(xcount < ycount){
@@ -137,10 +174,12 @@ public class PokerHands{
 			return 1; 
 		}
 	}
-	
-	//TODO: check if this works 
-	public int compareNumValue(int x, int y){
 
+	/*
+	 * Compares two numbers and returns -1 if x is less value than y, 0 if they are equal value and
+	 * 1 if y is greater value than x. 
+	 */
+	public static int compareNumValue(int x, int y){
 		int xcount = 0; 
 		int ycount = 0; 
 		boolean xfin = false;
@@ -164,11 +203,7 @@ public class PokerHands{
 				}
 			}
 		}
-		
-		
-	//	System.out.println("xcount "+ xcount);
-		//System.out.println("ycount "+ ycount);
-		
+	
 		if(xcount==ycount){
 			return 0; 
 		}else if(xcount < ycount){
@@ -177,78 +212,11 @@ public class PokerHands{
 			return 1; 
 		}
 	}
-	
 
-	public static void main(String[]args){
-		new PokerHands().start();
-	}
-	
-	public void start(){
-		Scanner sc = new Scanner(System.in);
-		while(sc.hasNext()){
-			String line = sc.nextLine(); 
-			ArrayList<Card<Integer, Character>> hand = new ArrayList<Card<Integer, Character>>();
-			Card<Integer,Character> card;
-			Boolean valid = true; 
-			String numString="";
-			//Random initialising. Must reset after each card! 
-			int num=0;
-			char suit='A'; 
-			//NOT SCANNING IN LAST ONE
-			
-			for(int i=0; i<line.length();i++){
-				char c = line.charAt(i); //scan in every char 
-				
-				if(Character.isDigit(c)){ 
-					
-					numString += c; //add to current number! could be double digit
-					num = Integer.parseInt(numString); //ISSUE
-					
-				}else if(nums.containsKey(c)){ //could be a num or the suit. 
-					num = nums.get(c); //ISSUE
-				}else if(suits.contains(Character.toUpperCase(c))){
-					suit = Character.toUpperCase(c);
-					/*if(!suits.contains(suit)){
-					//	valid = false; 
-					}*/ 
-					//TODO : check if valid suit
-				}else if(isSeperator(c)){
-				//	System.out.println("Found seperator");
-					card = new Card<Integer,Character>(num, suit); 
-					hand.add(card);
-					//reset?
-					num = 0;
-					numString = "";
-					suit = 'A';
-				}else{
-					valid = false; 
-				}
-			}
-			
-			if(hand.size()!=5 || !valid){
-				System.out.println("Invalid: " + line);
-				/*for(Card<Integer, Character> c: hand){
-					System.out.print(c.getNumber() + "" + c.getSuit()+" ");
-				}*/
-			}else{
-				System.out.println("Unsorted: ");
-				for(Card<Integer, Character> c: hand){
-					System.out.print(c.getNumber() + "" + c.getSuit()+" ");
-				}
-				
-				hand = sortHand(hand);
-				
-				System.out.println("\nSorted: ");
-				for(Card<Integer, Character> c: hand){
-					System.out.print(c.getNumber() + "" + c.getSuit()+" ");
-				}			
-			}				
-		}
-		sc.close();
-	}
-	
-		
-	public class Card<Integer, Character>{
+	/* 
+	 * A data structure which represents a single card and holds its number and suit. 
+	 */
+	public static class Card<Integer, Character>{
 		private final int number;
 		private final char suit; 
 		
